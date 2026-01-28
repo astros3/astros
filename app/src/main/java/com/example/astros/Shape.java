360945d45d20ad286472c0aa962b4fda5b2dc904
Tanmay Munjal <tanmaymunjal64@gmail.com>
2026-01-27 17:48:04 -0700

    added rectangle class

--- app/src/main/java/com/example/astros/Shape.java
+++ app/src/main/java/com/example/astros/Shape.java
@@ -1,7 +1,12 @@
 package com.example.astros;
 
-public abstract class Shape {
+abstract class Shape {
     protected int x;
     protected int y;
 }
 
+public class Rectangle extends Shape {
+    public int getArea() {
+        return x * y;
+    }
+}
