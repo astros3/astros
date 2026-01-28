0f3249a4d86ed456a50d5f4234c6ae2df7a12030
Tanmay Munjal <tanmaymunjal64@gmail.com>
2026-01-27 17:51:31 -0700

    separated files

--- app/src/main/java/com/example/astros/Shape.java
+++ app/src/main/java/com/example/astros/Shape.java
@@ -1,12 +1,6 @@
 package com.example.astros;
 
-abstract class Shape {
+public abstract class Shape {
     protected int x;
     protected int y;
-}
-
-public class Rectangle extends Shape {
-    public int getArea() {
-        return x * y;
-    }
 }
