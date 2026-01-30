9ab416e4a04dc49e72353c7b3b4efe73ba91e8fb
Mohammad Sadman Sharar Sayem <sadman@Mohammads-MacBook-Air-3.local>
2026-01-30 12:52:52 -0700

    Add color field to Shape

--- app/src/main/java/com/example/astros/Shape.java
+++ app/src/main/java/com/example/astros/Shape.java
@@ -3,6 +3,6 @@ package com.example.astros;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    protected String color = "Blue";
 }
 
